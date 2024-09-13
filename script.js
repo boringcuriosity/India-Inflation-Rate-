591736bf4319ec47e9451a59b417dd5201b9b074
Md Shaheer Ahmed <62714282+boringcuriosity@users.noreply.github.com>
2024-09-13 20:14:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndiaInflationChart from './IndiaInflationChart';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <IndiaInflationChart />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
