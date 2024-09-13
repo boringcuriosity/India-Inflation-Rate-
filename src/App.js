20b5debd106b3b7ceeca92e7948f5fb9000ff57c
Md Shaheer Ahmed <62714282+boringcuriosity@users.noreply.github.com>
2024-09-13 20:35:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IndiaInflationChart from './components/IndiaInflationChart';
+
+const App = () => {
+  return (
+    <div>
+      <h1>India Inflation Rate (2011-2024)</h1>
+      <IndiaInflationChart />
+    </div>
+  );
+};
+
+export default App;
